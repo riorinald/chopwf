af7ced2957c9957ea58d0c8222bc0f8fc9bba84e
rior <rio.rinldi@swiftx.co>
2019-11-17 19:37:45 +0700

    function convert Date

--- src/views/MyPendingTasks/MyPendingTasks.js
+++ src/views/MyPendingTasks/MyPendingTasks.js
@@ -140,6 +140,11 @@ class MyPendingTasks extends Component {
             })
     }
 
+    convertDate(dateValue){
+        let regEx = dateValue.replace(/(\d{4})(\d{2})(\d{2})/g, '$1,$2,$3')
+        return new Date(regEx);
+    }
+
     async getData(state, url) {
         try {
             const response = await Axios.get(url);
