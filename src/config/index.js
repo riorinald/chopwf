469d73c1eca7894aa8e1cdfacce78dece2fbad09
rior <riorinald@outlook.com>
2020-01-09 16:18:00 +0800

    Daimler staging url

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,5 @@
 export default {
-//     // url: "http://192.168.1.47/echopx/api/v1", //LOCAL-DEV
+    // url: "http://192.168.1.47/echopx/api/v1", //LOCAL-DEV
     url: "https://docms.es.corpintra.net/clwfb/api/v1", //DAIMLER
   };
 
