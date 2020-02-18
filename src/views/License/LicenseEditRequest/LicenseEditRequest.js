ddf08f943c62068493c3bc3dc0a65e889520584d
anandanairaan <anand@swiftx.co>
2020-02-18 17:39:03 +0800

    Changes: Added validation for export logs License

--- src/views/License/LicenseEditRequest/LicenseEditRequest.js
+++ src/views/License/LicenseEditRequest/LicenseEditRequest.js
@@ -141,6 +141,7 @@ class LicenseEditRequest extends Component {
         return regEx
     }
 
+
     async getTaskDetails(taskId) {
         this.setState({ loading: true })
         await Axios.get(`${config.url}/licenses/${taskId}?userId=${localStorage.getItem('userId')}`,
