61b9c91f813935d3b6282fb2511145e750beae88
Manoj <nishant.sonuley@bloomcs.com>
2020-02-25 15:50:34 +0530

    CPID-151: Added max char length for all the fields

--- src/views/Help/Help.js
+++ src/views/Help/Help.js
@@ -201,11 +201,11 @@ class Help extends Component {
         // this.setState({ chopKeepers: response.data.chopKeepers, QA: response.data.QA })
     }
 
-    async addNewChopKeeperDetails(details, index, name) {
+    addNewChopKeeperDetails(details, index, name) {
         let postData = new FormData()
         postData.append('sectionData', details)
         postData.append('sectionId', `${name}${index}`)
-        await Axios.post(`${config.url}/helps/chop/${localStorage.getItem('userId')}`, postData)
+        Axios.post(`${config.url}/helps/chop/${localStorage.getItem('userId')}`, postData)
             .then(result => {
                 console.log(`ADDED ${name}`)
             })
@@ -214,10 +214,10 @@ class Help extends Component {
             })
     }
 
-    async updateChopKeeperDetails(details, index, name) {
+    updateChopKeeperDetails(details, index, name) {
         let postData = new FormData()
         postData.append('sectionData', details)
-        await Axios.put(`${config.url}/helps/chop/${index}/${localStorage.getItem('userId')}`, postData)
+        Axios.put(`${config.url}/helps/chop/${index}/${localStorage.getItem('userId')}`, postData)
             .then(result => {
                 console.log(`UPDATED ${name}`)
             })
@@ -410,16 +410,16 @@ class Help extends Component {
             <tr key={index}>
                 <td>{table.chopType.map((type, i) =>
                     <div key={i}>
-                        <Form style={{ display: "flex" }}><Input type="text" onChange={this.handleChopKeeper("chopType", i, index)} placeholder="Please enter the chop type" defaultValue={type}></Input><Button onClick={() => this.deleteChopType(index, i)} color="danger">Delete</Button></Form><br />
+                        <Form style={{ display: "flex" }}><Input type="text" maxLength={1000} onChange={this.handleChopKeeper("chopType", i, index)} placeholder="Please enter the chop type" defaultValue={type}></Input><Button onClick={() => this.deleteChopType(index, i)} color="danger">Delete</Button></Form><br />
                     </div>
                 )}
                     <Button onClick={() => this.addChopType(index)} >Add New Chop Type</Button>
                 </td>
-                <td><Form><Input type="text" placeholder="Please enter the chop keeper" onChange={this.handleChange("chopKeeper", index)} defaultValue={table.chopKeeper}></Input></Form></td>
+                <td><Form><Input type="text" maxLength={1000} placeholder="Please enter the chop keeper" onChange={this.handleChange("chopKeeper", index)} defaultValue={table.chopKeeper}></Input></Form></td>
                 <td> {table.contactPerson.map((person, i) =>
-                    <div key={i}><Form style={{ display: "flex" }}><Input type="text" onChange={this.handleChopKeeper("contactPerson", i, index)} placeholder="Please enter the name of the contact person" defaultValue={person}></Input><Button onClick={() => this.deleteContactPerson(index, i)} color="danger">Delete</Button></Form><br /></div>)}
+                    <div key={i}><Form style={{ display: "flex" }}><Input type="text" maxLength={1000} onChange={this.handleChopKeeper("contactPerson", i, index)} placeholder="Please enter the name of the contact person" defaultValue={person}></Input><Button onClick={() => this.deleteContactPerson(index, i)} color="danger">Delete</Button></Form><br /></div>)}
                     <Button onClick={() => this.addContactPerson(index)} >Add New Contact Person</Button></td>
-                <td><Form><Input type="text" placeholder="Please add the location" onChange={this.handleChange("location", index)} defaultValue={table.location}></Input></Form></td>
+                <td><Form><Input type="text" maxLength={1000} placeholder="Please add the location" onChange={this.handleChange("location", index)} defaultValue={table.location}></Input></Form></td>
                 <td><Button onClick={() => this.deleteData(index)} color="danger">Delete</Button></td>
             </tr>)
         const QA = this.state.QA.map((qnsAns, index) =>
@@ -433,9 +433,9 @@ class Help extends Component {
                 <Form>
                     <Label style={{ float: "left" }}><b>Question: {index + 1}</b></Label>
                     <img style={{ float: "right" }} onClick={() => this.deleteQA(index)} height="20px" width="20px" src={deleteIcon} />
-                    <Input type="textarea" onChange={this.handleQAChange("question", index)} placeholder="Please enter your question." defaultValue={qnsAns.question}></Input>
+                    <Input type="textarea" maxLength={1999} onChange={this.handleQAChange("question", index)} placeholder="Please enter your question." defaultValue={qnsAns.question}></Input>
                     <Label><b>Answer</b></Label>
-                    <Input type="textarea" onChange={this.handleQAChange("answer", index)} placeholder="Please enter your answer." defaultValue={qnsAns.answer}></Input>
+                    <Input type="textarea" maxLength={1999} onChange={this.handleQAChange("answer", index)} placeholder="Please enter your answer." defaultValue={qnsAns.answer}></Input>
                 </Form>
                 <br />
             </div>)}<Button onClick={this.addQA}>Add New Question and Answer</Button></div>
