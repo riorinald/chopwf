b0028eb679f6c6c3e92b8076a3cacb87b691dd0b
rior <riorinald@outlook.com>
2020-03-03 14:14:57 +0800

    Changes: Logout message

--- src/views/Logout/Logout.js
+++ src/views/Logout/Logout.js
@@ -18,27 +18,29 @@ import Cookies from 'universal-cookie';
 const Logout = (props) => {
 	let [redirect, setRedirect] = useState(0);
 	const cookies = new Cookies();
+	const logout = () => setTimeout(setRedirect((redirect-1)),5000)
 	
-	const logout = () => setTimeout(setRedirect((redirect+1)),5000)
-
+	redirect = 1000
 	localStorage.clear()
 	cookies.remove('userInfo',{path:'/'})
 
 	fakeAuth.signOut(() => {logout()})
-
+	console.log(redirect)
 		return (<div style={{ backgroundColor: "#2F353A" }}>
-		{redirect === 0
+		{/* {redirect === 0
 		? <Redirect to='/login'/>
-		:	<>
+		:	<> */}
+
 				<Row className="centerd">
 					<Card className="shadow-lg p-3 rounded">
 						<CardBody>
-							<Alert color="danger">Successfully Logged out</Alert >
+							<Alert color="danger">Successfully Logged out the application.</Alert >
 							{/* <p className="mb-0"><center>Redirect in {timer}</center></p>  */}
 						</CardBody>
 					</Card>
 				</Row>
-			</>}
+			
+		{/* </>} */}
 		</div>
 		
 		)}
