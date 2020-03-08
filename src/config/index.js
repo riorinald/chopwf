150be435a395f7be657fa75f72ec739d0f4ba339
rior <riorinald@outlook.com>
2020-03-08 19:22:52 +0800

    Changes: OpenID config

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,3 @@
-
 //Daimler Staging OpenId client
 const scope ="openid"
 const client_id="812da7d2-b74a-484d-82a3-d30ff8ae6f9c"
@@ -6,11 +5,18 @@ const client_secret="5dd084f6-d9da-452a-86ee-45a6d301439f"
 const redirect_uri="https%3A%2F%2Fdocms.es.corpintra.net%2Fclwf%2Flogin%3Fauthhandler%3DDaimler_OpenID"
 const pathname=`https://sso-int.daimler.com/as/authorization.oauth2?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`
 
+//Daimler Production OpenId client
+// const scope ="openid"
+// const client_id="9eb69a21-65b0-4182-bc1e-0696f0efc294"
+// const client_secret="d4ca3fd2-cd88-480c-b47d-203f5e80c641"
+// const redirect_uri="https%3A%2F%2Fdocms.es.corpintra.net%2Fclwf%2Flogin%3Fauthhandler%3DDaimler_OpenID"
+// const pathname=`https://sso-int.daimler.com/as/authorization.oauth2?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}&scope=${scope}`
+
 export default {
-    //LOCAL-DEV
+    //LOCAL-DEV API
     url: "http://192.168.1.47/echopx/api/v1", 
     
-    //DAIMLER STAGING
+    //DAIMLER API
     // url: "https://docms.es.corpintra.net/clwfb/api/v1", 
     
     openid: pathname,
